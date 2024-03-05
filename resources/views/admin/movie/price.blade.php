9ff3316098fb48c74e90da4b6cf510f3600032be
kdnkcdkljour23jmflewf <guptadharmanshu@gmail.com>
2024-03-05 21:40:34 +0530

    MOvie price page with Audi data

--- resources/views/admin/movie/price.blade.php
+++ resources/views/admin/movie/price.blade.php
@@ -1,12 +1,44 @@
 @extends('admin.component.app')
 @section('content')
-@forelse ($movieprice_data as $item)
-                              @php
-                                  $item = (object) $item;
-                                @endphp
-                                {{$item->movies->name}}
-                                {{$item->category->name}}
-                                {{$item->price}}
-@empty
-@endforelse
+<table border="2">
+    <thead>
+        <tr>
+            <th>
+                Movie Name
+            </th>
+            <th>
+                Category Name
+            </th>
+            <th>
+                Movie Price
+            </th>
+            <th>
+                Auditoriam Name
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            @forelse ($movieprice_data as $item)
+                @php
+                    $item = (object) $item;
+                @endphp
+                @php
+                    $auditoriam_data = DB::table('auditoriam')->select('*')->whereIn('id',explode(',',$item->auditoriam_id))->get();
+                @endphp
+                    <td>{{$item->movies->name}} </td>
+                    <td>{{$item->category->name}}</td>
+                    <td>{{$item->price}}</td>
+                    <td>                
+                        @forelse($auditoriam_data as $v)
+                            {{ $v->name }}
+                            @empty
+                            No auditorium data available
+                        @endforelse
+                    </td>
+                @empty                                        
+            @endforelse
+        </tr>
+    </tbody>
+</table>
 @endsection
